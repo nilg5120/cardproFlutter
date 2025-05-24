4eb707fb43201a9ace3a8c63443bac4a8b513303
ohkubo <ohkubo5120@gmail.com>
2025-05-25 07:56:14 +0900

    feat: コンテナとデッキカード位置のテーブルを追加
    - 新しいファイル 'container_card_location.dart' を追加し、DeckCardLocations テーブルを定義
    - 新しいファイル 'cotainers.dart' を追加し、Containers テーブルを定義
    - 不要な 'deck.dart' と 'deck_card_location.dart' ファイルを削除

--- lib/db/container_card_location.dart
+++ lib/db/container_card_location.dart
@@ -1,10 +1,10 @@
 import 'package:drift/drift.dart';
 
 class DeckCardLocations extends Table {
-  TextColumn get deckId => text()();           // デッキID
+  IntColumn get containerId => integer().autoIncrement()();          // Containers.id
   TextColumn get cardInstanceId => text()();   // CardInstances.id
   TextColumn get location => text()();         // 'main', 'side' など
 
   @override
-  Set<Column> get primaryKey => {deckId, cardInstanceId, location};
+  Set<Column> get primaryKey => {containerId, cardInstanceId, location};
 }
